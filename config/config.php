63c7b22aa7ac6cd40a65716619aafcd2afaa1442
SinofPride-999 <jhaycodes999@gmail.com>
2025-12-24 22:16:56 +0000

    batman

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 
 return [
     'db' => [
-        'dsn' => $_ENV['DB_DSN'] ?? 'mysql:host=127.0.0.1;port=3306;dbname=luxid_test',
+        'dsn' => $_ENV['DB_DSN'] ?? 'mysql:host=127.0.0.1;port=3306;dbname=luxid',
         'user' => $_ENV['DB_USER'] ?? 'root',
         'password' => $_ENV['DB_PASSWORD'] ?? '',
     ],
