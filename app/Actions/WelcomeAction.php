1147267c6ea5e1f27190005be164be6a753afded
SinofPride-999 <jhaycodes999@gmail.com>
2026-02-16 18:05:21 +0000

    luxid/engine: v0.4.2

--- app/Actions/WelcomeAction.php
+++ app/Actions/WelcomeAction.php
@@ -2,12 +2,13 @@
 namespace App\Actions;
 
 use App\Actions\LuxidAction;
+use Luxid\Nodes\Nova;
 
 class WelcomeAction extends LuxidAction
 {
     public function index()
     {
-        return $this->nova('welcome', [
+        return Nova::render('welcome', [
             'title' => 'Welcome to Luxid Framework',
             'version' => '0.1.0',
             'phpVersion' => PHP_VERSION,
